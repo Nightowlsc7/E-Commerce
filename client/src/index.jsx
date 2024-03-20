7ebe7c181a876f5b03ba759ed530b4d7598c2998
badissaidane <saidanebadis1920@gmail.com>
2024-03-20 16:11:14 +0100

    done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,6 @@ import Justforyou from './components/Justforyou.jsx'
 import axios from 'axios'
 import NavBare from './components/NavBare.jsx'
 import Home from './components/Home.jsx'
-import $ from 'jquery'
 import Cart from './components/Cart.jsx'
 import Profile from './components/Profile.jsx'
 import SingUp from './components/SingUp.jsx'
@@ -18,7 +17,7 @@ const App = () => {
 
 
   useEffect(() => {
-   
+
   }, [])
 
 
@@ -28,29 +27,42 @@ const App = () => {
 
 
   // const SelectByCategory = (name) => {
-  //   axios.get(`http://localhost:3000/api/product/SearchByCategory/${name}`)
-    
+  //   axios.get(http://localhost:3000/api/product/SearchByCategory/${name})
+
   //     .then((res) =>   setData(res.data))
   //     .catch((err) => console.log(err))
   // } 
   //  const SelectByName = (name) => {
-  //   axios.get(`http://localhost:3000/api/product/SearchByName/${name}`)
-    
+  //   axios.get(http://localhost:3000/api/product/SearchByName/${name})
+
   //     .then((res) =>   setData(res.data))
   //     .catch((err) => console.log(err))
   // }
 
   // const selectOne = (id) => {
-  //   axios.get(`http://localhost:3000/api/product/SearchById/${id}`)
-    
+  //   axios.get(http://localhost:3000/api/product/SearchById/${id})
+
   //     .then((res) =>   setData(res.data))
   //     .catch((err) => console.log(err))
   // }
   return (
-    
+
     <div>
-      <h1>test</h1>
-    
+      <NavBare switchView ={switchView}/>
+
+       {view === 'Home' && <Home switchView={switchView} />}
+       {view === 'Contact' && <Home  />}
+       {view === 'About' && <About  />}
+       {view === 'Sign Up' && <Home  />}
+       {view === 'Cart' && <Cart  />}
+       {view === 'Wishlist' && <Wishlist  />}
+       {view === 'Profile' && <Profile  />}
+       {view === 'SingUp' && <SingUp  />}
+
+
+
+
+
     </div>
 
  
