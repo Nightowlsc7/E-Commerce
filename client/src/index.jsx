5b31460ab1b450fc91799d2cd77f49198416d7c3
RamySassi <rami9b3@live.fr>
2024-03-20 15:52:56 +0100

    y

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,12 @@ import About from './components/about.jsx'
 import Wishlist from './components/Wishlist.jsx'
 import Justforyou from './components/Justforyou.jsx'
 import axios from 'axios'
+import NavBare from './components/NavBare.jsx'
+import Home from './components/Home.jsx'
 import $ from 'jquery'
 import Cart from './components/Cart.jsx'
 import Profile from './components/Profile.jsx'
 import SingUp from './components/SingUp.jsx'
-
 
 
 const App = () => {
@@ -48,7 +49,20 @@ const App = () => {
   return (
     
     <div>
-      <h1>test</h1>
+      <NavBare switchView ={switchView}/>
+
+       {view === 'Home' && <Home switchView={switchView} />}
+       {view === 'Contact' && <Home  />}
+       {view === 'About' && <About  />}
+       {view === 'Sign Up' && <Home  />}
+       {view === 'Cart' && <Cart  />}
+       {view === 'Wishlist' && <Wishlist  />}
+       {view === 'Profile' && <Profile  />}
+       {view === 'SingUp' && <SingUp  />}
+
+
+       
+
     
     </div>
 
